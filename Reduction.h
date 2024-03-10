eebeb7a0cbee5615d266f905fc0b9fa85761aff0
Serge Rogatch <serge.rogatch@gmail.com>
2024-03-10 10:00:36 +0100

    Fix the reduction

--- Reduction.h
+++ Reduction.h
@@ -6,25 +6,27 @@
 struct Reduction {
   Formula formula_;
   Graph fGraph_; // formula graph
-  int64_t iRoot_;
+  int64_t halfNVertices_;
 
   explicit Reduction(const Formula& src) : formula_(src) {
-    iRoot_ = formula_.nVars_ + formula_.nClauses_ + 1;
-    // Edges between variables and their negations
+    halfNVertices_ = formula_.nVars_ + formula_.nClauses_;
+
+    // Undirected edges between variables and their negations
     for(int64_t i=1; i<=formula_.nVars_; i++) {
       fGraph_.AddReplace(Arc(-i, i, 0, formula_.nClauses_));
       fGraph_.AddReplace(Arc(i, -i, 0, formula_.nClauses_));
     }
-    // Edges between clauses and the root
+
+    // Directed clause edges
     for(int64_t i=1; i<=formula_.nClauses_; i++) {
-      fGraph_.AddReplace(Arc(formula_.nVars_ + i, iRoot_, 1, formula_.nClauses_));
-      fGraph_.AddReplace(Arc(iRoot_, formula_.nVars_ + i, 1, formula_.nClauses_));
+      fGraph_.AddReplace(Arc(-formula_.nVars_ - i, formula_.nVars_ + i, 1, formula_.nVars_));
     }
+
     // Edges between variables/negations, and the clauses
     for(const auto& clause : formula_.clause2var_) {
       for(const int64_t iVar : clause.second) {
-        fGraph_.AddReplace(Arc(clause.first + formula_.nVars_, iVar, 0, formula_.nClauses_));
-        fGraph_.AddReplace(Arc(iVar, clause.first + formula_.nVars_, 0, formula_.nClauses_));
+        fGraph_.AddReplace(Arc(iVar, -formula_.nVars_ - clause.first, 1, formula_.nVars_));
+        fGraph_.AddReplace(Arc(formula_.nVars_ + clause.first, -iVar, 1, formula_.nVars_));
       }
     }
   }
