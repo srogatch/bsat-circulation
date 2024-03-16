09688c9b53c2a4c2ce8e9c01fb18ace791400505
Serge Rogatch <serge.rogatch@gmail.com>
2024-03-16 18:19:52 +0100

    Add build instructions

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# sudo apt-get install libjemalloc-dev
+# sudo apt-get install libjemalloc-dev libtbb-dev
 mkdir -p bin
 g++-11 -g BSAT.cpp -std=c++20 -march=native -fopenmp -ltbb -ljemalloc -o bin/BSAT-Debug
 g++-11 -ggdb3 -O3 -funroll-loops -ffast-math \
