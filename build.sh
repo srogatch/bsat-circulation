c26902d85ec030fb10496b199faa901834ab73dd
Serge Rogatch <serge.rogatch@gmail.com>
2024-03-22 09:27:27 +0100

    Improve the build command for profiling

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ clang++-17 BSAT.cpp -DNDEBUG -O3 -funroll-loops -ffast-math -march=native \
   -std=c++20 -fopenmp -Wl,--no-as-needed -ldl -ljemalloc -ltbb -o bin/Rogasat
 clang++-17 BSAT.cpp -g -O3 -funroll-loops -ffast-math -march=native \
   -std=c++20 -fopenmp -Wl,--no-as-needed -ldl -ljemalloc -ltbb -o bin/BSAT-Release
-clang++-17 BSAT.cpp -g -O2 -fno-omit-frame-pointer -ffast-math -march=native \
+clang++-17 BSAT.cpp -gdwarf-4 -O2 -fno-omit-frame-pointer -ffast-math -march=native \
   -std=c++20 -fopenmp -Wl,--no-as-needed -ldl -ljemalloc -ltbb -o bin/BSAT-Profiling
 clang++-17 BSAT.cpp -ggdb3 -std=c++20 -march=native -fopenmp -Wl,--no-as-needed \
   -ldl -ljemalloc -ltbb -Wall -Wextra -o bin/BSAT-Debug
