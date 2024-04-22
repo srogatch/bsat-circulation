7ab21506e550aeecb231632fd1ab1f22c23bf15f
Serge Rogatch <serge.rogatch@gmail.com>
2024-04-22 15:57:50 +0200

    A fix for false triangular assumption

--- CpuSolver.h
+++ CpuSolver.h
@@ -88,58 +88,75 @@ struct CpuSolver {
       const VCIndex aClause = ieSys.clauseMap_[i];
       assert(1 <= aClause && aClause <= pFormula_->nClauses_);
       ieSys.c_[i] = 2 - pFormula_->clause2var_.ArcCount(aClause); // - double(satTr.nSat_[aClause]);
-      for(VCIndex j=0; j<ieSys.VarCount(); j++) {
-        const VCIndex aVar = ieSys.varMap_[j];
-        if(pFormula_->clause2var_.HasArc(aClause, aVar)) {
-          ieSys.A_[i][j] = 1;
-        } else if(pFormula_->clause2var_.HasArc(aClause, -aVar)) {
-          ieSys.A_[i][j] = -1;
+      for(int8_t sign=-1; sign<=1; sign+=2) {
+        const VCIndex nArcs = pFormula_->clause2var_.ArcCount(aClause, sign);
+        for(VCIndex j=0; j<nArcs; j++) {
+          const VCIndex iVar = pFormula_->clause2var_.GetTarget(aClause, sign, j);
+          const VCIndex aVar = llabs(iVar);
+          assert(ieSys.varMap_[aVar-1] == aVar);
+          if(iVar < 0) {
+            ieSys.A_[i][aVar-1] = -1;
+          } else {
+            ieSys.A_[i][aVar-1] = 1;
+          }
         }
       }
+      // for(VCIndex j=0; j<ieSys.VarCount(); j++) {
+      //   const VCIndex aVar = ieSys.varMap_[j];
+      //   if(pFormula_->clause2var_.HasArc(aClause, aVar)) {
+      //     ieSys.A_[i][j] = 1;
+      //   } else if(pFormula_->clause2var_.HasArc(aClause, -aVar)) {
+      //     ieSys.A_[i][j] = -1;
+      //   }
+      // }
     }
 
     constexpr const double eps = 1e-10;
 
+    std::vector<VCIndex> leadRow(ieSys.VarCount(), -1);
+    VCIndex nLeads = 0;
     for(VCIndex i=0; i<ieSys.VarCount(); i++) {
-      VCIndex j=i;
+      VCIndex j=nLeads;
       for(; j<ieSys.ClauseCount(); j++) {
-        if(fabs(ieSys.A_[j][i]) > eps) {
+        if( fabs(ieSys.A_[j][i]) > eps ) {
           break;
         }
       }
       if(j >= ieSys.ClauseCount()) {
         continue;
       }
-      if(i != j) {
-        std::swap(ieSys.c_[i], ieSys.c_[j]);
-        ieSys.A_[i].swap(ieSys.A_[j]);
-        std::swap(ieSys.clauseMap_[i], ieSys.clauseMap_[j]);
+      if(nLeads != j) {
+        std::swap(ieSys.c_[nLeads], ieSys.c_[j]);
+        ieSys.A_[nLeads].swap(ieSys.A_[j]);
+        std::swap(ieSys.clauseMap_[nLeads], ieSys.clauseMap_[j]);
       }
-      const double rev = 1.0 / fabs(ieSys.A_[i][i]);
+      leadRow[i] = nLeads;
+      nLeads++;
+      const double rev = 1.0 / fabs(ieSys.A_[leadRow[i]][i]);
       for(VCIndex j=0; j<ieSys.VarCount(); j++) {
-        ieSys.A_[i][j] *= rev;
+        ieSys.A_[leadRow[i]][j] *= rev;
       }
-      ieSys.c_[i] *= rev;
+      ieSys.c_[leadRow[i]] *= rev;
       for(j=0; j<ieSys.ClauseCount(); j++) {
-        if(j == i || fabs(ieSys.A_[j][i]) <= eps) {
+        if(j == leadRow[i] || fabs(ieSys.A_[j][i]) <= eps) {
           continue;
         }
-        if(Signum(ieSys.A_[j][i]) == Signum(ieSys.A_[i][i])) {
+        if(Signum(ieSys.A_[j][i]) == Signum(ieSys.A_[leadRow[i]][i])) {
           // No problem to assign
           continue;
         }
-        const double mul = - ieSys.A_[j][i] / ieSys.A_[i][i];
+        const double mul = - ieSys.A_[j][i] / ieSys.A_[leadRow[i]][i];
         assert(mul >= 0);
         #pragma omp parallel for num_threads(nSysCpus) schedule(guided, kRamPageBytes/sizeof(double))
         for(VCIndex k=0; k<ieSys.VarCount(); k++) {
           if(k == i) {
-            assert( fabs(ieSys.A_[j][k] + mul * ieSys.A_[i][k]) <= eps );
+            assert( fabs(ieSys.A_[j][k] + mul * ieSys.A_[leadRow[i]][k]) <= eps );
             ieSys.A_[j][k] = 0; // Avoid floating-point flaws / epsilons
             continue;
           }
-          ieSys.A_[j][k] += mul * ieSys.A_[i][k];
+          ieSys.A_[j][k] += mul * ieSys.A_[leadRow[i]][k];
         }
-        ieSys.c_[j] += mul * ieSys.c_[i];
+        ieSys.c_[j] += mul * ieSys.c_[leadRow[i]];
       }
     }
 
