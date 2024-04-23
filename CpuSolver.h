6e8f40e16e1c56d28e63a22838df9293a6949d92
Serge Rogatch <serge.rogatch@gmail.com>
2024-04-24 00:55:55 +0200

    Outputs look better

--- CpuSolver.h
+++ CpuSolver.h
@@ -98,7 +98,7 @@ struct CpuSolver {
       for(VCIndex i=0; i<pFormula_->nVars_; i++) {
         // coeff*y[i]*y[i]
         smtP.emplace_back(startY+i, startY+i, 1);
-        optQ[startY+i] = -0.5;
+        optQ[startY+i] = -1;
         // y[i]*z[i] = (1-x[i])*(1+x[i]) = 1 - x[i]*x[i]
         smtP.emplace_back(startY+i, startZ+i, 1);
         // x[i] = 1 - y[i]  <=>  x[i]+y[i] = 1
@@ -111,7 +111,7 @@ struct CpuSolver {
       for(VCIndex i=0; i<pFormula_->nVars_; i++) {
         // coeff * z[i] * z[i]
         smtP.emplace_back(startZ+i, startZ+i, 1);
-        optQ[startZ+i] = -0.5;
+        optQ[startZ+i] = -1;
         // x[i] = z[i] - 1  <=>  x[i] - z[i] = -1
         smtA.emplace_back(pFormula_->nClauses_ + startZ + i, i, 1); // x[i]
         smtA.emplace_back(pFormula_->nClauses_ + startZ + i, startZ+i, -1); // z[i]
