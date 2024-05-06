ca3df08d470ec00929124221def21d593df54f07
Serge Rogatch <serge.rogatch@gmail.com>
2024-05-06 16:17:14 +0200

    Converges for 20 vars

--- julia/BsatLp.jl
+++ julia/BsatLp.jl
@@ -56,7 +56,6 @@ println("Number of variables: ", n_variables)
 println("Clauses: ", clauses)
 setprecision(BigFloat, n_variables*2)
 
-var_order = collect(1:n_variables)
 A_rows = Int[]
 A_cols = Int[]
 A_vals = BigFloat[]
@@ -77,7 +76,7 @@ for a_var in 1:n_variables
   push!(A_vals, BigFloat(1))
   l[i_constr] = -1
   u[i_constr] = 1
-  q[i_constr] = 2.0^(-i_constr)
+  q[i_constr] = BigFloat(2.0) ^ (-i_constr)
 end
 
 for clause in clauses
@@ -93,13 +92,60 @@ for clause in clauses
 end
 
 A = sparse(A_rows, A_cols, A_vals)
-@show q
-@show l
-@show u
 
 model = COSMO.Model{BigFloat}()
 constraint = COSMO.Constraint(A, zeros(BigFloat, n_variables+length(clauses)), COSMO.Box(l, u))
-settings = COSMO.Settings{BigFloat}(verbose = true)
+settings = COSMO.Settings{BigFloat}(verbose = true, max_iter=Int(1e10))
 assemble!(model, P, q, constraint, settings = settings)
-result = COSMO.optimize!(model);
-# println(result)
+
+const eps = 1e-3
+maybe_sat = true
+tot_its = 0;
+while maybe_sat
+  global maybe_sat, tot_its
+  tot_its += 1
+  x_warm = Vector{BigFloat}(undef, n_variables)
+  for i in 1:n_variables
+    if asgs[i]
+      x_warm[i] = 1
+    else
+      x_warm[i] = -1
+    end
+  end
+  COSMO.warm_start_primal!(model, x_warm)
+  lp_res = COSMO.optimize!(model);
+  if lp_res.status != :Solved && lp_res.status != :SolvedSuboptimal
+    maybe_sat = false
+    println("UNSATISFIABLE: ", lp_res.status)
+    break
+  end
+  x_sol = lp_res.x
+  def_vars = []
+  undef_vars = []
+  for i in 1:n_variables
+    val = x_sol[i]
+    if abs(val) < 1-eps
+      push!(undef_vars, i)
+    else
+      push!(def_vars, i)
+      if val > -0
+        asgs[i] = true
+      else
+        asgs[i] = false
+      end
+    end
+  end
+  if length(undef_vars) == 0
+    println("SATISFIED")
+    break
+  end
+  sort!(undef_vars, lt=(a, b) -> abs(x_sol[a]) < abs(x_sol[b]))
+  sort!(def_vars, lt=(a, b) -> abs(x_sol[a]) < abs(x_sol[b]))
+  var_ord = [undef_vars; def_vars]
+  for i in 1:n_variables
+    q[var_ord[i]] = BigFloat(2.0) ^ (-i)
+  end
+  update!(model, q = q)
+end
+
+println("Number of top-level iterations: ", tot_its)
